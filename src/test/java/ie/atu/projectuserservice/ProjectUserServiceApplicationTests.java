7c0771b86d7da0871c66eb6ef83fb46c4f98ddd7
Gavin <G00402296@atu.ie>
2023-12-31 13:49:29 +0000

    Inital Commit

---
+++ src/test/java/ie/atu/projectuserservice/ProjectUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.projectuserservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectUserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
